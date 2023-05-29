d200fb8b933598e5a161ef5558155482e6db1fb7
bingenk <s10244655@connect.np.edu.sg>
2023-05-29 11:42:56 +0800

    initial commit (Homepage)

---
+++ app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package sg.edu.np.mad.mad2023_team2.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
