7f80eea398333f20c9f0b0491865e878dd5259f2
bingenk <s10244655@connect.np.edu.sg>
2023-06-25 16:15:23 +0800

    About Us page implemented

--- app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/home/SettingsActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/home/SettingsActivity.java
@@ -1,9 +1,10 @@
 package sg.edu.np.mad.mad2023_team2.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import sg.edu.np.mad.mad2023_team2.R;
@@ -27,10 +28,24 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            Preference aboutUsPreference = findPreference("contact_preference");
+            aboutUsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    openAboutUsActivity();
+                    return true;
+                }
+            });
+        }
+
+        private void openAboutUsActivity() {
+            Intent intent = new Intent(getActivity(), AboutUsActivity.class);
+            startActivity(intent);
         }
     }
-
 }
