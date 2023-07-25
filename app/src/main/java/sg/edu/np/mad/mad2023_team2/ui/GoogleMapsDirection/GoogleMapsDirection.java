f28af229dd1a6ba6a5981e49538fbae6ff085ae5
bingenk <s10244655@connect.np.edu.sg>
2023-07-25 16:45:34 +0800

    Google Map Direction side Navigation tab added for activity (Stage 2)

---
+++ app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/GoogleMapsDirection/GoogleMapsDirection.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.mad2023_team2.ui.GoogleMapsDirection;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import sg.edu.np.mad.mad2023_team2.R;
+
+public class GoogleMapsDirection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps_direction);
+    }
+}
