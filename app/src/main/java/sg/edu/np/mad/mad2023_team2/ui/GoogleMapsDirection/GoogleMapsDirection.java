38f27159c4e42a492181d0cc69c018c5fadf510b
bingenk <s10244655@connect.np.edu.sg>
2023-07-27 01:05:57 +0800

    Google Map Direction show current location implemented (Stage 2)

--- app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/GoogleMapsDirection/GoogleMapsDirection.java
+++ app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/GoogleMapsDirection/GoogleMapsDirection.java
@@ -1,38 +1,86 @@
 package sg.edu.np.mad.mad2023_team2.ui.GoogleMapsDirection;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 import sg.edu.np.mad.mad2023_team2.R;
 
-public class GoogleMapsDirection extends AppCompatActivity implements OnMapReadyCallback {
+public class GoogleMapsDirection extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap myMap;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_maps_direction);
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int LOCATION_REQUEST_CODE = 100;
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_direction, container, false);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        return view;
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-
-        //Initialize googleMap
+        // Initialize googleMap
         myMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request permission
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else {
+            getLocationAndUpdateMap();
+        }
+    }
 
-        LatLng Singapore = new LatLng(1.290270,103.851959);
-        myMap.addMarker(new MarkerOptions().position(Singapore).title("Singapore"));
-        myMap.moveCamera(CameraUpdateFactory.newLatLng(Singapore));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocationAndUpdateMap();
+            }
+        }
+    }
+
+    private void getLocationAndUpdateMap() {
+        if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                        myMap.addMarker(new MarkerOptions().position(userLocation).title("You Are Here"));
+                        myMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                    }
+                }
+            });
+        }
     }
 }
