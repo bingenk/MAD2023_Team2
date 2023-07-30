e7cd90ef85d631459005a59870bdc2124faeef12
bingenk <s10244655@connect.np.edu.sg>
2023-07-30 17:27:29 +0800

    Modifying home page and added features for the home page

--- app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad2023_team2/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
@@ -23,6 +24,9 @@ import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -36,12 +40,15 @@ import java.util.concurrent.ExecutionException;
 
 import sg.edu.np.mad.mad2023_team2.R;
 import sg.edu.np.mad.mad2023_team2.databinding.ActivityMainBinding;
+import sg.edu.np.mad.mad2023_team2.databinding.FragmentAccomodationsBinding;
+import sg.edu.np.mad.mad2023_team2.ui.BookingAttraction.BookAttractActivity;
 import sg.edu.np.mad.mad2023_team2.ui.Cart.ApiManager;
 import sg.edu.np.mad.mad2023_team2.ui.Cart.Cart;
 import sg.edu.np.mad.mad2023_team2.ui.Cart.CartDataCallback;
 import sg.edu.np.mad.mad2023_team2.ui.Cart.DeleteAllManager;
 import sg.edu.np.mad.mad2023_team2.ui.Currency_Converter.SetCurrencyDetailsManager;
 import sg.edu.np.mad.mad2023_team2.ui.LoginSignup.Login;
+import sg.edu.np.mad.mad2023_team2.ui.accomodations.AccommodationsFragment;
 import sg.edu.np.mad.mad2023_team2.ui.cart_sqllite_database.DataBaseHelper;
 import sg.edu.np.mad.mad2023_team2.ui.cart_sqllite_database.DatabaseManager;
 import sg.edu.np.mad.mad2023_team2.ui.checkout_cart_sqllite.Cart_item;
@@ -60,36 +67,42 @@ public class MainActivity extends AppCompatActivity {
 
     // For user to logout upon clicking logout button and return to login page
     private void logoutUser() {
-        mAuth.signOut();
-        Intent intent = new Intent(MainActivity.this, Login.class);
-        startActivity(intent);
-        //PRAVEEN CODE
-        // Delete cart data upon user logout
-       dataBaseHelper = DatabaseManager.getDataBaseHelper(MainActivity .this);
-        // get username from shared prefs to use in firebase
-        SharedPreferences sharedPreferences = getSharedPreferences("CartFb", MODE_PRIVATE);
-        String username = sharedPreferences.getString("username", ""); // The second argument is the default value if the key is not found
-        Log.d("wassup", "ShowCustomersOnListView: firebase cart mfker");
-        Log.d("wassup", username);
+        new AlertDialog.Builder(this)
+                .setTitle("Logout Confirmation")
+                .setMessage("Are you sure you want to logout?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mAuth.signOut();
+                        Intent intent = new Intent(MainActivity.this, Login.class);
+                        startActivity(intent);
+                        // Delete cart data upon user logout
+                        dataBaseHelper = DatabaseManager.getDataBaseHelper(MainActivity.this);
+                        // get username from shared prefs to use in firebase
+                        SharedPreferences sharedPreferences = getSharedPreferences("CartFb", MODE_PRIVATE);
+                        String username = sharedPreferences.getString("username", ""); // The second argument is the default value if the key is not found
+                        Log.d("wassup", "ShowCustomersOnListView: firebase cart mfker");
+                        Log.d("wassup", username);
 
-        checkout_cart_details details=this.dataBaseHelper.getEveryone();
-        addCartToUser(username,details.getAllcartitems());
+                        checkout_cart_details details = dataBaseHelper.getEveryone();
+                        addCartToUser(username, details.getAllcartitems());
 
-        SetCurrencyDetailsManager.getInstance(getApplicationContext()).resetApiCalled();
-        DeleteAllManager.getInstance(getApplicationContext()).makeDeleteCall();
-        ApiManager.getInstance(getApplicationContext()).resetApiCalled();
-        DeleteAllManager.getInstance(getApplicationContext()).resetApiCalled();// Reset apiCalled variable
+                        SetCurrencyDetailsManager.getInstance(getApplicationContext()).resetApiCalled();
+                        DeleteAllManager.getInstance(getApplicationContext()).makeDeleteCall();
+                        ApiManager.getInstance(getApplicationContext()).resetApiCalled();
+                        DeleteAllManager.getInstance(getApplicationContext()).resetApiCalled();// Reset apiCalled variable
 
-
-//        if (variable > 0) {
-//            Toast.makeText(MainActivity.this,"Successfully logout, cart details is cleared.", Toast.LENGTH_SHORT).show();
-//        } else {
-//            Toast.makeText(MainActivity.this,"Successfully logout.", Toast.LENGTH_SHORT).show();
-//        }
-        finish();
-
-        //////////
+//                    if (variable > 0) {
+//                        Toast.makeText(MainActivity.this,"Successfully logout, cart details is cleared.", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        Toast.makeText(MainActivity.this,"Successfully logout.", Toast.LENGTH_SHORT).show();
+//                    }
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -112,7 +125,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         /// PRAVEEN CODE
         //delete all the data in the memory
         DeleteAllManager.getInstance(getApplicationContext()).makeDeleteCall();
 
@@ -121,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
 
         //make the api call to get the cart from the api
         ApiManager.getInstance(getApplicationContext()).makeApiCall();
-        /////
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -129,11 +140,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-
-
-
-
-//        dataBaseHelper = DatabaseManager.getDataBaseHelper(MainActivity.this);
 
         // Show user profile upon when user login
         // Get references to the profileUsername and profileEmail TextViews
@@ -167,6 +173,55 @@ public class MainActivity extends AppCompatActivity {
             public boolean onMenuItemClick(MenuItem item) {
                 logoutUser();
                 return true;
+            }
+        });
+
+        //Home Page Navigation
+        CardView hotelBookingCard = findViewById(R.id.card_hotel_booking);
+        CardView attractionCard = findViewById(R.id.card_Attraction);
+        CardView googleMapsDirectionCard = findViewById(R.id.card_GoogleMapsDirection);
+        CardView translateCard = findViewById(R.id.card_Translate);
+        CardView logoutCard = findViewById(R.id.card_logout);
+        hotelBookingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to Accomodation
+                NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_accommodations);
+            }
+        });
+
+        attractionCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the Accomodation Activity
+                Intent intent = new Intent(MainActivity.this, BookAttractActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        googleMapsDirectionCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to GoogleMapsDirection
+                NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_direction);
+            }
+        });
+
+        translateCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to Translate
+                NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_translate);
+            }
+        });
+
+        logoutCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
             }
         });
 
@@ -226,13 +281,6 @@ public class MainActivity extends AppCompatActivity {
                 .addOnSuccessListener(aVoid -> Log.d("wassup", "addCartToUser: Database write was successful."))
                 .addOnFailureListener(e -> Log.d("wassup", "addCartToUser: Database write failed: " + e.getMessage()));
     }
-
-
-
-
-
-
-
     /// PRAVEEN CODE
     public int deletealldata()
     {   dataBaseHelper = DatabaseManager.getDataBaseHelper(MainActivity .this);
@@ -240,13 +288,13 @@ public class MainActivity extends AppCompatActivity {
         // Assuming you are using getDefaultSharedPreferences
         SharedPreferences sharedPreferences1 = PreferenceManager.getDefaultSharedPreferences(this);
 
-// Get the editor
+        // Get the editor
         SharedPreferences.Editor editor = sharedPreferences1.edit();
 
-// Clear the SharedPreferences
+        // Clear the SharedPreferences
         editor.clear();
 
-// Apply the changes
+        // Apply the changes
         editor.apply();
     return checkloggedout;
 
